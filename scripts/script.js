6e0f898b01735788f72d9e4edbc739ca9195552f
James Milne <jmilne22@gmail.com>
2017-05-15 04:09:28 -0400

    First commit

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".who-i-am").typed({
+    strings: ["Software Developer", "Linux Enthusiast", "Musician"],
+			typeSpeed: 100,
+      loop: true,
+      cursorChar: "_"
+  });
+});
